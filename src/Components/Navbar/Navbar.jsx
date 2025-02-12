b519df2914e7f9845e59e196efacb1744b9c7a7b
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-02-12 18:54:23 +0600

    updated the size of the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
 
   return (
     <div className="bg-white sticky top-0 z-50">
-      <div className="navbar  container  mx-auto items-center">
+      <div className="navbar px-4  items-center">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
