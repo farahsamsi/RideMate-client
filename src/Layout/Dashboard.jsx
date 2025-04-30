0172b7da4519f3b71c0e3d2fa5e15143050efb01
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-04-30 18:52:00 +0600

    added dashboard side bar

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,17 @@
+import { useContext, useEffect, useState } from "react";
+import { IoMdMenu } from "react-icons/io";
+import { AuthContext } from "../Components/AuthProvider/AuthProvider";
 const Dashboard = () => {
+  const { user, loading } = useContext(AuthContext);
+  console.log(user);
+  const [newUser, setNewUser] = useState({});
+  useEffect(() => {
+    if (loading) {
+      console.log("wait");
+    } else {
+      setNewUser(user);
+    }
+  }, [loading, user]);
   return (
     <div>
       {/* drawer navbar for dashboard */}
@@ -6,12 +19,29 @@ const Dashboard = () => {
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center">
           {/* Page content here */}
-          <label
-            htmlFor="my-drawer-2"
-            className="btn btn-primary drawer-button lg:hidden"
-          >
-            Open drawer
-          </label>
+          {/* navbar for medium and small device */}
+          <div className="flex w-full justify-between px-7 py-4  lg:hidden">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-11 rounded-full">
+                <img
+                  referrerPolicy="no-referrer"
+                  alt={newUser?.displayName}
+                  src={newUser?.photoURL}
+                />
+              </div>
+            </div>
+            <label
+              htmlFor="my-drawer-2"
+              className="btn bg-primary drawer-button lg:hidden"
+            >
+              {/* Open drawer */}
+              <IoMdMenu />
+            </label>
+          </div>
         </div>
         <div className="drawer-side">
           <label
@@ -21,6 +51,21 @@ const Dashboard = () => {
           ></label>
           <ul className="menu bg-base-200 text-base-content min-h-full w-52 md:w-72 lg:w-80 p-4">
             {/* Sidebar content here */}
+            <li>
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar w-full h-fit flex items-center justify-center"
+              >
+                <div className="w-11 lg:w-40 rounded-full">
+                  <img
+                    referrerPolicy="no-referrer"
+                    alt={newUser?.displayName}
+                    src={newUser?.photoURL}
+                  />
+                </div>
+              </div>
+            </li>
             <li>
               <a>Sidebar Item 1</a>
             </li>
